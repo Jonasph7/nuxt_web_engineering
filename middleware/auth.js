4afd70f12c483bf9f5eeaaeb44cba62efc122044
JohannesLks <74617774+JohannesLks@users.noreply.github.com>
2024-05-26 12:52:05 +0200

    init

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,7 @@
-// middleware/auth.js
-import { defineNuxtRouteMiddleware, navigateTo } from '#app'
+export default defineNuxtRouteMiddleware((to, _from) => {
+  const user = useSupabaseUser()
 
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const response = await fetch('https://fh-kiel.com/check_auth.php', {
-    method: 'GET',
-    credentials: 'include' // WICHTIG: Sendet Cookies mit
-  })
-  const isAuthenticated = await response.json()
-
-  if (!isAuthenticated && to.path !== '/login') {
-    return navigateTo('/login')
+  if (!user.value) {
+      return navigateTo('/login')
   }
 })
---
+++ pages/backend.vue
@@ -0,0 +1,32 @@
+<script setup>
+    definePageMeta({
+        // middleware: 'auth'
+    })
+    const user = ref(useSupabaseUser())
+    const supabase = useSupabaseClient()
+    const { data: kommentare } = await supabase.from("Bewerbungen").select("*")
+</script>
+
+<template>
+    <div>
+        <h1>Backend</h1>
+        <p v-if="user">Hallo {{ user.email }}</p>
+        <p>Hier nur Zugang mit Mail und Password</p>
+        <div class="">
+            <div v-for="k in kommentare">
+                <p>{{ k }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
