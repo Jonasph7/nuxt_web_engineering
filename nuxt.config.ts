5ac691b49a9393c76e56db15d99998963163846d
JohannesLks <74617774+JohannesLks@users.noreply.github.com>
2024-05-28 08:22:14 +0200

    working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // nuxt.config.js
 export default {
   router: {
-    middleware: ['auth']
-  }
+    middleware: ['auth']},
+    modules: ['@nuxtjs/supabase'],
+    supabase: {
+      url: 'https://xdqwolnmgphskookqgdp.supabase.co',
+      key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkcXdvbG5tZ3Boc2tvb2txZ2RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyODgwNzEsImV4cCI6MjAzMTg2NDA3MX0.hSM_WdaOj3Dz6QUhc0e9QDYl_FSN1lDcDaE6fQvdEn0'
+    }
 }
--- pages/backend.vue
+++ pages/backend.vue
@@ -1,32 +1,39 @@
 <script setup>
-    definePageMeta({
-        // middleware: 'auth'
-    })
-    const user = ref(useSupabaseUser())
-    const supabase = useSupabaseClient()
-    const { data: kommentare } = await supabase.from("Bewerbungen").select("*")
+
+
+const user = ref(null)
+const kommentare = ref([])
+
+const supabase = useSupabaseClient()
+const supabaseUser = useSupabaseUser()
+
+onMounted(async () => {
+  user.value = supabaseUser.value
+  const { data } = await supabase.from('Bewerbungen').select('*')
+  kommentare.value = data
+})
 </script>
 
 <template>
+  <div>
+    <h1>Backend</h1>
+    <p v-if="user">Hallo {{ user.email }}</p>
+    <p>Hier nur Zugang mit Mail und Password</p>
     <div>
-        <h1>Backend</h1>
-        <p v-if="user">Hallo {{ user.email }}</p>
-        <p>Hier nur Zugang mit Mail und Password</p>
-        <div class="">
-            <div v-for="k in kommentare">
-                <p>{{ k }}</p>
-            </div>
-        </div>
+      <div v-for="k in kommentare" :key="k.id">
+        <p>{{ k }}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 h1 {
-    margin-bottom: 20px;
-    font-size: 36px;
+  margin-bottom: 20px;
+  font-size: 36px;
 }
 
 p {
-    margin: 20px 0;
+  margin: 20px 0;
 }
 </style>
