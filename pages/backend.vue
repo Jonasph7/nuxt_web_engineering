9a5d11a747cddfffba5bd721e9f27bb2e5716a9e
JohannesLks <74617774+JohannesLks@users.noreply.github.com>
2024-06-13 13:12:09 +0200

    rot korrigiert

--- pages/backend.vue
+++ pages/backend.vue
@@ -149,7 +149,7 @@
         <div class="bg-white dark:bg-gray-800 p-6 border shadow-lg rounded-lg max-w-lg w-full" @click.stop>
           <h2 class="text-2xl font-bold text-gray-900 dark:text-gray-100">Are you sure you want to delete this ticket?</h2>
           <div class="mt-4 flex justify-between">
-            <button @click="deleteTicket(selectedTicketId)" class="bg-red-500 text-white px-4 py-2 rounded-full">Delete</button>
+            <button @click="deleteTicket(selectedTicketId)" class="bg-red text-white px-4 py-2 rounded-full">Delete</button>
             <button @click="cancelDelete" class="bg-gray-700 dark:bg-gray-600 text-white px-4 py-2 rounded-full">Cancel</button>
           </div>
         </div>
