a7db067235e91b50b30c621558b382c4c12223c7
JohannesLks <74617774+JohannesLks@users.noreply.github.com>
2024-06-13 13:20:42 +0200

    farbe angepasst

--- components/Kalender.vue
+++ components/Kalender.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container mx-auto p-4">
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div v-for="event in calendarEvents" :key="event.id" class="p-4 border rounded-lg">
+        <div v-for="event in calendarEvents" :key="event.id" class="p-4 border rounded-lg dark:bg-gray-800">
           <h2 class="text-lg font-semibold">{{ event.title }}</h2>
           <p>Date: {{ formatDateTime(event.date) }}</p>
           <button @click="$emit('confirm-delete-event', event.id)" class="mt-2 px-4 py-2 bg-red text-white rounded-full">Delete</button>
